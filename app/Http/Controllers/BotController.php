b52d132b546e69bc6e1a8f3d300645e1fc560005
Daniel Dauber <daniel@portalqualis.com.br>
2019-12-04 14:47:16 -0300

    atualizacao do sdk

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -7,7 +7,10 @@ use CodeBot\WebHook;
 use CodeBot\SenderRequest;
 use CodeBot\Message\Text;
 use CodeBot\Element\Button;
+use CodeBot\Element\Product;
 use CodeBot\TemplatesMessage\ButtonsTemplate;
+use CodeBot\TemplatesMessage\GenericTemplate;
+use CodeBot\TemplatesMessage\ListTemplate;
 use Illuminate\Http\Request;
 
 class BotController extends Controller
@@ -29,15 +32,39 @@ class BotController extends Controller
         $senderId = $sender->getSenderId();
         $message = $sender->getMessage();
 
-        $text = new Text($senderId);
+        $text = new Text((int) $senderId);
         $callSendApi = new CallSendApi(config('botfb.pageAcessToken'));
 
         $callSendApi->make($text->message('Olá, voce digitou' . $message));
         // $callSendApi->make($text->message('Você digitou', $message));
 
-        $message = new ButtonsTemplate($senderId);
+        $message = new ButtonsTemplate((int) $senderId);
         $message->add(new Button('web_url', 'Google', 'https://www.google.com'));
         $callSendApi->make($message->message('Texto do botão'));
+
+        $product = new Product(
+            'Produto',
+            'https://www.idealmarketing.com.br/blog/wp-content/uploads/2018/09/marketing-de-produto-conceito.jpg',
+            'Subtitulo do Produto',
+            new Button('web_url', null, 'http://www.google.com')
+        );
+
+        $template = new ListTemplate(1234);
+        $template->add($product);
+
+        $callSendApi->make($template->message('message'));
+
+        $product = new Product(
+            'Produto',
+            'https://www.idealmarketing.com.br/blog/wp-content/uploads/2018/09/marketing-de-produto-conceito.jpg',
+            'Subtitulo do Produto',
+            new Button('web_url', null, 'http://www.google.com')
+        );
+
+        $template = new GenericTemplate(1234);
+        $template->add($product);
+
+        $callSendApi->make($template->message('message'));
 
 
         return '';
